d08d45ec1aec8afb7267ce2858a70ec9a19701fe
alchemyyy <github@grassmail.net>
2026-07-05 19:15:46 -0700

    resharper cleanup

--- TrayAppDotNETCommon/src/UI/Hotkeys/HotkeyKeys.cs
+++ TrayAppDotNETCommon/src/UI/Hotkeys/HotkeyKeys.cs
@@ -36,36 +36,31 @@ public static class TrayAppDotNETHotkeyKeys
 
     public static string KeyName(uint vk)
     {
-        switch (vk)
+        return vk switch
         {
-            case >= 0x41 and <= 0x5A:
-            case >= 0x30 and <= 0x39:
-                return ((char)vk).ToString();
-            case >= 0x60 and <= 0x69:
-                return "Num " + (vk - 0x60).ToString(CultureInfo.InvariantCulture);
-            case >= 0x70 and <= 0x87:
-                return "F" + (vk - 0x6F).ToString(CultureInfo.InvariantCulture);
-            default:
-                return vk switch
-                {
-                    0x08 => "Backspace",
-                    0x09 => "Tab",
-                    0x0D => "Enter",
-                    0x1B => "Esc",
-                    0x20 => "Space",
-                    0x21 => "Page Up",
-                    0x22 => "Page Down",
-                    0x23 => "End",
-                    0x24 => "Home",
-                    0x25 => "Left",
-                    0x26 => "Up",
-                    0x27 => "Right",
-                    0x28 => "Down",
-                    0x2D => "Insert",
-                    0x2E => "Delete",
-                    _ => "VK " + vk.ToString(CultureInfo.InvariantCulture)
-                };
-        }
+            >= 0x41 and <= 0x5A or >= 0x30 and <= 0x39 => ((char)vk).ToString(),
+            >= 0x60 and <= 0x69 => "Num " + (vk - 0x60).ToString(CultureInfo.InvariantCulture),
+            >= 0x70 and <= 0x87 => "F" + (vk - 0x6F).ToString(CultureInfo.InvariantCulture),
+            _ => vk switch
+            {
+                0x08 => "Backspace",
+                0x09 => "Tab",
+                0x0D => "Enter",
+                0x1B => "Esc",
+                0x20 => "Space",
+                0x21 => "Page Up",
+                0x22 => "Page Down",
+                0x23 => "End",
+                0x24 => "Home",
+                0x25 => "Left",
+                0x26 => "Up",
+                0x27 => "Right",
+                0x28 => "Down",
+                0x2D => "Insert",
+                0x2E => "Delete",
+                _ => "VK " + vk.ToString(CultureInfo.InvariantCulture)
+            }
+        };
     }
 
     public static string ModifierText(uint modifiers)
